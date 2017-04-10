d02596064bf134cbb008c03b9f7d103c32d7511c
Koo12 <506637238@qq.com>
2017-04-10 15:40:06 +0800

    chushikuangjia

---
+++ StudentManage.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.sql.*;
+public class StudentManage extends JFrame implements ActionListener {
+	JPanel mb1,mb2;
+	JLabel bq1,bq2;
+	JTextField wbk;
+	JButton an1;
+	public static void main(String[] args){
+		StudentManage xx=new StudentManage();
+	}
+	StudentManage()
+	{
+		
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		
+	}
+}
