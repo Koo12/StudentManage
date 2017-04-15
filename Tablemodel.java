0967168730acbab8a7ee1d1624afd3c858a1653f
Koo12 <506637238@qq.com>
2017-04-15 21:15:04 +0800

    在模型中直接添加

--- Tablemodel.java
+++ Tablemodel.java
@@ -3,25 +3,41 @@ import java.util.Vector;
 import javax.swing.JTextField;
 import javax.swing.table.*;
 public class Tablemodel extends AbstractTableModel{
-	public int getRowCount()
-	{
-		return 1;
-	}
-	public int getColumnCount()
-	{
-		return 1;
-	}
-	public Object getValueAt(int row,int column)
-	{
-		return this;
-	}
 	
 	Vector Title,xiang;
 	PreparedStatement ps=null;
 	Connection ct=null;
 	ResultSet rs=null;
 	
+	public int getRowCount()
+	{
+		return this.xiang.size();
+	}
+    public int getColumnCount()
+    {
+		return this.Title.size();
+	}
+	public Object getValueAt(int hang, int lie)
+	{
+		return ((Vector)this.xiang.get(hang)).get(lie);
+	}
+	
+	Tablemodel(JTextField wbk2,String sql)
+	{
+		this.moxing(wbk2,sql);
+	}
+	
 	Tablemodel(JTextField wbk2)
+	{
+		String sql="select * from data";
+		this.moxing(wbk2,sql);	
+	}
+	public String getColumnName(int e)//让每一列的名称显示出来
+	{
+		return (String)this.Title.get(e);
+	}
+	
+	public void moxing(JTextField wbk2,String sql)
 	{
 		Title=new Vector();
 		Title.add("学号");
@@ -38,7 +54,7 @@ public class Tablemodel extends AbstractTableModel{
 		{
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			ct=DriverManager.getConnection("jdbc:sqlserver://localhost:1433;DatabaseName=transcript","sa","gzx254052834843");
-			ps=ct.prepareStatement("select * from data");
+			ps=ct.prepareStatement(sql);
 			rs=ps.executeQuery();
 			
 			while(rs.next())
